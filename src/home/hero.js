dc19a5c38049e97ba0cca1c2b14769ca8b5f3f86
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-01 05:34:45 -0300

    Add hero animation

--- src/home/hero.js
+++ src/home/hero.js
@@ -1,8 +1,12 @@
+const hero = "#hero";
 const heroScrollIndicator = ".hero-scroll-icon-wrapper";
 const highlightedText = ".hero-content>h2>span";
 const highlightedTextElement = document.querySelector(highlightedText);
+const fish1 = ".fish-1";
+const fish2 = ".fish-2";
 
 gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
 const heroScrollIndicatorTween = gsap.to(heroScrollIndicator, {
   duration: 2,
@@ -26,3 +30,45 @@ highlightTimeline
   .to(highlightedText, { duration: 1, opacity: 1 });
 
 updateText();
+
+gsap.fromTo(
+  fish1,
+  {
+    scrollTrigger: {
+      trigger: hero,
+      scrub: true,
+      start: "top center",
+    },
+    x: 300,
+  },
+  {
+    scrollTrigger: {
+      trigger: hero,
+      scrub: true,
+      start: "top center",
+    },
+    x: -400,
+  }
+);
+
+gsap.fromTo(
+  fish2,
+  {
+    scrollTrigger: {
+      trigger: hero,
+      scrub: true,
+      start: "top center",
+    },
+    y: 120,
+    x: -1000,
+  },
+  {
+    scrollTrigger: {
+      trigger: hero,
+      scrub: true,
+      start: "top center",
+    },
+    x: 100,
+    y: 50,
+  }
+);
