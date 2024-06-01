527fa23d696faa00c339edc06c547fb3a6f1fd67
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-01 18:29:49 -0300

    Start section 1 background animation

--- src/home/section-1.js
+++ src/home/section-1.js
@@ -1,4 +1,24 @@
 const section1 = "#section-1";
+const animationBackgroundCircle = ".section-1-background>svg>.main-circle";
+const section1BackgroundSvg = document.querySelector(
+  ".section-1-background>svg"
+);
+const svgHeight = section1BackgroundSvg.height.baseVal.value;
+
+ScrollTrigger.create({
+  trigger: section1,
+  start: "top 0",
+  end: "bottom 0",
+  scrub: 0.001,
+  animation: gsap.to(
+    animationBackgroundCircle,
+    {
+      y: svgHeight - 500,
+    },
+    0
+  ),
+  scroller: "main",
+});
 
 ScrollTrigger.create({
   trigger: section1,
